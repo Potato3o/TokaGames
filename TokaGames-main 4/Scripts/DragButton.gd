99ad928bd6ab14126792b175f7b440c77c95b055
TheNateTheory101 <37158780+TheNateTheory101@users.noreply.github.com>
2023-05-18 08:18:57 -0400

    5/18 backup - functioning caterpillar with updated sprites

--- TokaGames-main 4/Scripts/DragButton.gd
+++ TokaGames-main 4/Scripts/DragButton.gd
@@ -5,13 +5,20 @@ extends TouchScreenButton
 
 
 
-var colors = ["res://caterpillar/redCircle.png","res://caterpillar/blueCircle.png","res://caterpillar/yellowCircle.png"]
+var colorsX = [368.391,370.163,368.391]
+var colorsY = [183.73,18.974,569.161]
+var colorsW = [122.238,122.238,124.009]
+var colorsH = [134.639,138.182,136.41]
+var atlas
 var index = 0
 var x = 1
 func _caterpillar(i):
 	for a in range(i):
+		atlas = AtlasTexture.new()
 		var circle = Sprite2D.new()
-		circle.texture = load(colors[index])
+		atlas.atlas = load("res://caterpillarSprites.png")
+		atlas.region = Rect2(colorsX[index],colorsY[index],colorsW[index],colorsH[index],)
+		circle.texture = atlas
 		index = index+1
 		if index == 3:
 			index = 0
@@ -23,3 +30,4 @@ func _caterpillar(i):
 func _on_pressed():
 	#$"../levels menu".enabled = true
 	#$"../levels menu".make_current()
+	_caterpillar(1)
