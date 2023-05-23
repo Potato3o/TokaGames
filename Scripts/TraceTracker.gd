6d508c4ab12468dd548ee9fa261fef18b79012a9
Potato3o <105457810+Potato3o@users.noreply.github.com>
2023-05-23 08:27:24 -0400

    Completed trace funvtion
    0-4 blue cards have extra small error zones to prevent scribbling

--- Scripts/TraceTracker.gd
+++ Scripts/TraceTracker.gd
@@ -7,10 +7,14 @@ var max = 0
 var count = 0
 var checkBox = []
 
+var buttonArray = []
 #redraws these vectors 
 var vectorArray = []
 
-
+var startpoint = Vector2.ZERO
+var stoppoint = Vector2.ZERO
+var countpoint = 0
+var countdown = 0 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
@@ -29,6 +33,7 @@ func _ready():
 #resets all arrays and counts
 func clearOut():
 	vectorArray.clear()
+	buttonArray.clear()
 	checkBox.fill(false)
 	count = 0
 
@@ -40,7 +45,7 @@ func tracepressed():
 		if child.is_pressed() && (checkBox[i] == false):
 			count += 1
 			checkBox[i] = true
-			vectorArray.append(child.position)
+			buttonArray.append(child.position)
 			queue_redraw()
 	if count >= max:
 		get_parent().get_parent().cardNumber += 1
@@ -49,7 +54,29 @@ func tracepressed():
 		queue_free()
 
 func _draw():
+	for v in buttonArray:
+		draw_circle(v,10,Color.RED)
 	for i in vectorArray:
-		draw_circle(i,10,Color.YELLOW)
+		draw_line(i[0],i[1],Color.YELLOW,5)
+		
+func _input(event):
+	if event is InputEventScreenDrag:
+		var local_event = make_input_local(event)
+		stoppoint = local_event.position
+		if countpoint == 0:
+			startpoint = local_event.position
+			stoppoint = Vector2(local_event.position.x, local_event.position.y+1)
+		queue_redraw()
+		var array = [startpoint,stoppoint]
+		vectorArray.append(array)
+		startpoint = stoppoint
+		countpoint = 1
+		countdown = 3
+	if (event is InputEventScreenTouch) && (event.is_pressed()):
+		pass # start touch which means start drag
+	else:
+		countdown -= 1
+		if countdown ==0:
+			countpoint = 0
 
 
